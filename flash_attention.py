3415504fa3bdf2c12e1fea09b13de6283fd76ac1
SPThole <spthole2@gmail.com>
2026-02-15 21:00:44 +0530

    adding original logic

--- flash_attention.py
+++ flash_attention.py
@@ -43,6 +43,43 @@ def final_softmax(tile_stats, global_max):
         row = row + v['tile']*torch.exp(v['local_max']-global_max).unsqueeze(-1) 
     return row / sums.unsqueeze(-1)
 
+def flash_attention(Q, K, V, tile_size=2):
+    B, N, S, D = Q.shape
+    QKV = torch.zeros(B,N,S,D)
+    for qi in range(0, Q.shape[-2], tile_size):
+        # print('qi',qi)
+        l_max = torch.ones(B,N,tile_size)*-float('inf')
+        g_max = torch.ones(B,N,tile_size)*-float('inf')
+        Q_chunk = Q[:,:,qi:qi+tile_size,:]
+        l_sum = torch.zeros(B,N,tile_size,1)
+        QKV_chunk = torch.zeros(B,N,tile_size,D)
+        old_gmax = torch.ones_like(g_max)
+        for ki in range(0,K.shape[-2],tile_size):
+
+            K_chunk = K[:,:,ki:ki+tile_size,:] # B, N, T, D
+            V_chunk = V[:,:,ki:ki+tile_size,:] # B, N, T, D
+            QK_chunk = Q_chunk@K_chunk.transpose(-2,-1) # B, N, T, T
+            
+            l_max = torch.max(QK_chunk,dim=-1).values # B,N,T
+            stack_max = torch.stack((l_max, g_max), dim=-1) # B,N,T,2
+
+            g_max = torch.max(stack_max,dim=-1).values # B,N,T
+            
+            QK_chunk = QK_chunk - g_max.unsqueeze(-1) # B, N, T , T - B, N, T,1 
+            QK_chunk = torch.exp(QK_chunk) # B, N, T, T
+            
+            scaling = torch.exp(old_gmax-g_max).unsqueeze(-1) # B, N, T, 1
+            old_gmax = g_max
+            contribution_from_earlier_tile = l_sum*scaling # B, N, T, 1 * B, N, T, 1 -> B, N, T, 1
+            l_sum = torch.sum(QK_chunk,dim=-1,keepdim=True) + contribution_from_earlier_tile # B, N, T, 1
+            
+            QKV_contribution_from_earlier_tile = QKV_chunk*scaling # B, N, T, D * B, N, T, 1 -> B, N, T, D
+            # this is like adding the contribution from earlier tile in getting V earlier, it was only first tile*softmax weight was added
+            QKV_chunk = QK_chunk@V_chunk + QKV_contribution_from_earlier_tile # B, N, T, T mult B, N, T, D -> B, N, T, D
+        QKV[:,:,qi:qi+tile_size,:] = QKV_chunk / l_sum # B, N, T, D / B, N, T, 1 -> B, N, T, D
+    return QKV
+
+
 
 if __name__ == "__main__":
     a = torch.randn(8,10,8,8)
@@ -56,3 +93,11 @@ if __name__ == "__main__":
     QKV2 = QK2@V
     print(QKV2)
     print(torch.allclose(s,QKV2,atol=1e-5))
+
+    Q = torch.randn((8,12,10,64))
+    K = torch.randn((8,12,10,64))
+    V = torch.randn((8,12,10,64))
+    oo = flash_attention(Q,K,V, tile_size=2)
+    QK2 = torch.softmax(Q@K.transpose(-2,-1),dim=-1)
+    QKV2 = QK2@V
+    print(torch.allclose(oo,QKV2,atol=1e-5))
