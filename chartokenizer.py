1e2e5765ab01ed63d12514d47203df92de5bc853
SPThole <spthole2@gmail.com>
2026-05-01 19:08:01 +0530

    added unk token handling encode decode

--- chartokenizer.py
+++ chartokenizer.py
@@ -1,5 +1,5 @@
 from tqdm import tqdm
-
+import torch
 
 class CharTokenize:
     def __init__(self):
@@ -9,15 +9,28 @@ class CharTokenize:
                               'bos_token':'<BOS>',
                               'unk_token':'<UNK>'
                               }
+        self.vocab: dict[str, int] = {}
+        self.inv_vocab: dict[int, str] = {}
 
     def tokenize(self,text):
         return list(text)
 
-    def encode(self, text):
+    def encode(self, text, return_torch=False, max_len=None, pad=False):
         unk_id = self.vocab[self.special_tokens['unk_token']]
-        return [self.vocab.get(t, unk_id) for t in text]
+        ids = [self.vocab.get(t, unk_id) for t in text]
+
+        if max_len is not None:
+            if len(ids) > max_len:
+                ids = ids[:max_len]
+            elif pad:
+                pad_id = self.vocab[self.special_tokens['pad_token']]
+                ids.extend([pad_id] * (max_len - len(ids)))
+        
+        if return_torch:
+            return torch.tensor(ids, dtype=torch.long)
+        return ids
     
-    def decode(self,tokens):
+    def decode(self, tokens: list[int]) -> str:
         return "".join([self.inv_vocab[t] for t in tokens])
 
     def train(self, texts):
